1f6c0bcabf43eac3c44950f704df4e0f970c08c6
emilyxxie <emilyxxie@gmail.com>
2016-01-23 16:50:57 -0500

    okay, it works kind of

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@
 var symbols = [];
 var g = 200;
 var b = 60;
+var symbolsLength;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
@@ -13,22 +14,15 @@ function setup() {
 }
 
 function draw() {
+  console.log(symbols.length);
   background(0);
-
-  for (var j = 0; j < 40; j++) { 
+  for (var i = 0; i < symbolsLength; i++) {
+    var symbol = symbols[i];
     fill(symbol.r, symbol.g, symbol.b);
     textSize(24);
-    var symbol = symbols[j];
-    text(symbol.character);
-    for (var i = 0; i < symbolsLength; i++) {
-      // var symbol = symbols[i];
-      // fill(symbol.r, symbol.g, symbol.b);
-      // textSize(24);
-      // text(symbol.character, symbol.x, symbol.y);
-      // symbol.convertSymbol();
-      // symbol.scroll();
-      // symbol.convertInterval++;
-    }
+    text(symbol.character, symbol.x, symbol.y);
+    symbol.scroll();
+    symbol.convertSymbol();
   }
 }
 
@@ -36,23 +30,28 @@ function createSymbolStream() {
   var yStart = 0;
   var xStart = 0;
 
-  // set up a line of symbols
-  for (var i = 0; i < random(15, 40); i++) {
-    if (yStart == 0) {
-      symbols.push(
-        new Symbol(xStart, yStart, 255, 255, 255)
-      );
-    } else {
-      symbols.push(
-        new Symbol(xStart, yStart, 0, g, b)
-      );
-      g -= 8;
-      b -= 8;
+  for (var j = 0; j < width / 24; j++) {
+    startG = g;
+    startB = b;
+    for (var i = 0; i < random(15, 40); i++) {
+      if (yStart == 0) {
+        symbols.push(
+          new Symbol(xStart, yStart, 255, 255, 255)
+        );
+      } else {
+        symbols.push(
+          new Symbol(xStart, yStart, 0, g, b)
+        );
+        g -= 8;
+        b -= 8;
+      }
+      yStart -= 24;
     }
-    yStart -= 24;
+    g = startG;
+    b = startB;
+    xStart += 24;
+    yStart = 0;
   }
-  xStart += 24;
-  yStart = 0;
 }
 
 
@@ -73,7 +72,7 @@ function Symbol(x, y, r, g, b) {
   this.convertInterval = Math.round(random(0, 400));
 
   this.scroll = function() {
-    this.y += 2;
+    this.y += 8;
   }
 
   this.convertSymbol = function() {
